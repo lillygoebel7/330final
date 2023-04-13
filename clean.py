8f095cfa57d657b0297c6d3715eeec6ed1a4f60e
Tyler <tylhuang@umich.edu>
2023-04-13 17:27:03 -0400

    push

--- clean.py
+++ clean.py
@@ -1,15 +1,11 @@
 # list to store file lines
 lines = []
 # read file
-with open("se-mi-reddit-comments.json", 'r') as fp:
-    # read an store all lines into list
+with open("se-mi-reddit-comments.jsonl", 'r') as fp:
     lines = fp.readlines()
 
 # Write file
 with open(r"se-mi-reddit-comments-short.json", 'w') as fp:
-    # iterate each line
     for number, line in enumerate(lines):
-        # delete line 5 and 8. or pass any Nth line you want to remove
-        # note list index starts from 0
-        if number < 86750:
+        if number % 17 == 0:
             fp.write(line)
